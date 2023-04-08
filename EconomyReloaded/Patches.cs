acde92fcebfe2ea896506aea8c1f8dc3a1538622
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-04-08 10:15:56 +1000

    Add HarmonyPatch attribute to Patches class in EconomyReloaded/Patches.cs file.

--- EconomyReloaded/Patches.cs
+++ EconomyReloaded/Patches.cs
@@ -4,6 +4,7 @@ using HarmonyLib;
 
 namespace EconomyReloaded;
 
+[HarmonyPatch]
 public static class Patches
 {
     private static bool _gameBalanceAlreadyRun;
@@ -14,24 +15,38 @@ public static class Patches
     [HarmonyPatch(typeof(Trading), nameof(Trading.GetSingleItemCostInTraderInventory), typeof(Item), typeof(int))]
     public static void Trading_GetSingleItemCostInTraderInventory(ref float __result, Item item)
     {
-        if (!Plugin.OldSchoolModeConfig.Value) return;
-        if (!Plugin.DisableInflationConfig.Value) return;
-        if (__result != 0.0)
+        if (!Plugin.Inflation.Value)
         {
-            __result = item.definition.base_price;
+            if (__result != 0.0)
+            {
+                __result = item.definition.base_price;
+            }
+
+            Plugin.Log.LogWarning(
+                $"GetSingleItemCostInTraderInventory: Inflation is off, returning base price: {item.definition.base_price}");
         }
+
+        Plugin.Log.LogWarning(
+            $"GetSingleItemCostInTraderInventory: Inflation is on, returning normal price: {__result}");
     }
 
     [HarmonyPostfix]
     [HarmonyPatch(typeof(Trading), nameof(Trading.GetSingleItemCostInPlayerInventory), typeof(Item), typeof(int))]
     public static void Trading_GetSingleItemCostInPlayerInventory(ref float __result, Item item)
     {
-        if (!Plugin.OldSchoolModeConfig.Value) return;
-        if (!Plugin.DisableDeflationConfig.Value) return;
-        if (__result != 0.0)
+        if (!Plugin.Deflation.Value)
         {
-            __result = item.definition.base_price;
+            if (__result != 0.0)
+            {
+                __result = item.definition.base_price;
+            }
+
+            Plugin.Log.LogWarning(
+                $"GetSingleItemCostInPlayerInventory: Deflation is off, returning base price: {item.definition.base_price}");
         }
+
+        Plugin.Log.LogWarning(
+            $"GetSingleItemCostInPlayerInventory: Deflation is on, returning normal price: {__result}");
     }
 
     public static void RestoreIsStaticCost()
@@ -40,11 +55,23 @@ public static class Patches
         {
             itemDef.is_static_cost = BackedUpIsStaticCost[itemDef.id];
         }
+
+        Plugin.Log.LogWarning($"Restored {BackedUpIsStaticCost.Count} items to their original is_static_cost value.");
     }
-    
+
+    public static void MakeIsStaticCost()
+    {
+        foreach (var itemDef in GameBalance.me.items_data.Where(itemDef => StaticCostItemIds.Contains(itemDef.id)))
+        {
+            itemDef.is_static_cost = true;
+        }
+
+        Plugin.Log.LogWarning($"Set {BackedUpIsStaticCost.Count} items to is_static_cost = true.");
+    }
+
     public static void GameBalance_LoadGameBalance(GameBalance obj)
     {
-        if (Plugin.OldSchoolModeConfig.Value || _gameBalanceAlreadyRun) return;
+        if (_gameBalanceAlreadyRun) return;
         _gameBalanceAlreadyRun = true;
 
         var itemsWithBasePrice = GameBalance.me.items_data.Where(itemDef => itemDef.base_price > 0).ToList();
