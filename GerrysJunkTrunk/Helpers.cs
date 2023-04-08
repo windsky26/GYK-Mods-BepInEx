13e2e5d46df5ca70a20552a5c906c787b1a15c7e
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-04-08 12:34:57 +1000

    Refactor Helpers.cs to improve readability and maintainability.

--- GerrysJunkTrunk/Helpers.cs
+++ GerrysJunkTrunk/Helpers.cs
@@ -5,7 +5,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using GerrysJunkTrunk.lang;
 using GYKHelper;
-using HarmonyLib;
 using UnityEngine;
 
 namespace GerrysJunkTrunk;
@@ -18,7 +17,8 @@ public partial class Plugin
         ItemDefinition.ItemType.Pickaxe, ItemDefinition.ItemType.FishingRod, ItemDefinition.ItemType.BodyArmor,
         ItemDefinition.ItemType.HeadArmor, ItemDefinition.ItemType.Sword, ItemDefinition.ItemType.Preach,
         ItemDefinition.ItemType.GraveStone, ItemDefinition.ItemType.GraveFence, ItemDefinition.ItemType.GraveCover,
-        ItemDefinition.ItemType.GraveStoneReq, ItemDefinition.ItemType.GraveFenceReq, ItemDefinition.ItemType.GraveCoverReq,
+        ItemDefinition.ItemType.GraveStoneReq, ItemDefinition.ItemType.GraveFenceReq,
+        ItemDefinition.ItemType.GraveCoverReq,
     };
 
     private static string GetLocalizedString(string content)
@@ -31,16 +31,14 @@ public partial class Plugin
     {
         if (error)
         {
-            Log.LogError($"{message}");
+            Log.LogError(message);
         }
-        else
+        else if (_debug.Value)
         {
-            if (_debug.Value)
-            {
-                Log.LogInfo($"{message}");
-            }
+            Log.LogInfo(message);
         }
     }
+
 
     private static async void ShowSummary(string money)
     {
@@ -52,7 +50,8 @@ public partial class Plugin
         {
             Thread.CurrentThread.CurrentUICulture = CrossModFields.Culture;
 
-            salesByVendor.AddRange(_vendorSales.Select(vendor => vendor.GetSales().OrderBy(a => a.GetItem().id).ToList()));
+            salesByVendor.AddRange(
+                _vendorSales.Select(vendor => vendor.GetSales().OrderBy(a => a.GetItem().id).ToList()));
         });
 
         var resultBuilder = new StringBuilder();
@@ -63,26 +62,34 @@ public partial class Plugin
                 sale.GetItem().GetItemName(), strings.For, Trading.FormatMoney(sale.GetPrice()));
         }
 
-        await Task.Run(() => { GUIElements.me.dialog.OpenOK($"[37ff00]{strings.Header}[-]", null, $"{resultBuilder}", true, $"{money}"); });
+        await Task.Run(() =>
+        {
+            GUIElements.me.dialog.OpenOK($"[37ff00]{strings.Header}[-]", null, $"{resultBuilder}", true,
+                $"{money}");
+        });
     }
 
     private static void ClearGerryFlag(ref ChestGUI chestGui)
     {
         if (chestGui == null || !_usingShippingBox || StackSizeBackups.Count <= 0) return;
 
-        foreach (var inventory in new[] {chestGui.player_panel.multi_inventory.all[0].data.inventory, chestGui.chest_panel.multi_inventory.all[0].data.inventory})
+        var inventories = new[]
         {
-            foreach (var item in inventory)
+            chestGui.player_panel.multi_inventory.all[0].data.inventory,
+            chestGui.chest_panel.multi_inventory.all[0].data.inventory
+        };
+
+        foreach (var item in inventories.SelectMany(inventory => inventory))
+        {
+            if (StackSizeBackups.TryGetValue(item.id, out var value))
             {
-                if (StackSizeBackups.TryGetValue(item.id, out var value))
-                {
-                    item.definition.stack_count = value;
-                }
+                item.definition.stack_count = value;
             }
         }
 
         _usingShippingBox = false;
     }
+
 
     private static float GetBoxEarnings(WorldGameObject shippingBox)
     {
@@ -144,7 +151,8 @@ public partial class Plugin
             {
                 for (var i = 0; i < totalCount; i++)
                 {
-                    var itemCost = Mathf.Round(myTrader.GetSingleItemCostInPlayerInventory(selectedItem, -i) * 100f) / 100f;
+                    var itemCost = Mathf.Round(myTrader.GetSingleItemCostInPlayerInventory(selectedItem, -i) * 100f) /
+                                   100f;
                     num += itemCost;
                 }
             }
@@ -173,7 +181,9 @@ public partial class Plugin
 
     private static void ShowIntroMessage()
     {
-        GUIElements.me.dialog.OpenOK(strings.Message1, null, $"{strings.Message2}\n{strings.Message3}\n{strings.Message4}\n{strings.Message5}\n{strings.Message6}\n{strings.Message7}", true, strings.Message8);
+        GUIElements.me.dialog.OpenOK(strings.Message1, null,
+            $"{strings.Message2}\n{strings.Message3}\n{strings.Message4}\n{strings.Message5}\n{strings.Message6}\n{strings.Message7}",
+            true, strings.Message8);
     }
 
     private static void StartGerryRoutine(float num)
@@ -181,71 +191,78 @@ public partial class Plugin
         Thread.CurrentThread.CurrentUICulture = CrossModFields.Culture;
         var noSales = num <= 0;
         var money = Trading.FormatMoney(num, true);
-        //var gerry = WorldMap.GetNPCByObjID("talking_skull");
-        var gerry = WorldMap.SpawnWGO(_shippingBox.transform, "talking_skull", new Vector3(_shippingBox.pos3.x, _shippingBox.pos3.y + 43f, _shippingBox.pos3.z));
-        Tools.NameSpawnedGerry(gerry);
-        gerry.ReplaceWithObject("talking_skull", true);
-        Tools.NameSpawnedGerry(gerry);
+        var gerry = SpawnGerry(_shippingBox.transform, _shippingBox.pos3);
 
-        GJTimer.AddTimer(2f, delegate
-        {
-            gerry.Say(noSales ? strings.Nothing : strings.WorkWork, delegate
+        GJTimer.AddTimer(2f,
+            delegate
             {
-                GJTimer.AddTimer(1f, delegate
-                {
-                    gerry.ReplaceWithObject("talking_skull", true);
-                    Tools.NameSpawnedGerry(gerry);
-                    gerry.DestroyMe();
-                });
-            }, null, SpeechBubbleGUI.SpeechBubbleType.Talk, SmartSpeechEngine.VoiceID.Skull);
-        });
+                gerry.Say(noSales ? strings.Nothing : strings.WorkWork, delegate { DestroyGerryWithDelay(gerry, 1f); });
+            });
 
         if (noSales) return;
+
         GJTimer.AddTimer(8f, delegate
         {
-            var gerry2 = WorldMap.SpawnWGO(_shippingBox.transform, "talking_skull", new Vector3(_shippingBox.pos3.x, _shippingBox.pos3.y + 43f, _shippingBox.pos3.z));
-            Tools.NameSpawnedGerry(gerry2);
-            gerry2.ReplaceWithObject("talking_skull", true);
-            Tools.NameSpawnedGerry(gerry2);
+            var gerry2 = SpawnGerry(_shippingBox.transform, _shippingBox.pos3);
 
             GJTimer.AddTimer(2f, delegate
             {
                 gerry2.Say($"{money}", delegate
+                {
+                    _shippingBox.data.inventory.Clear();
+                    PlayCoinsSoundAndShowMessage(num, gerry2, money);
+
+                    GJTimer.AddTimer(2f, delegate
                     {
-                        _shippingBox.data.inventory.Clear();
-                        if (_showSoldMessagesOnPlayer.Value)
+                        gerry2.Say(strings.Bye, delegate
                         {
-                            Sounds.PlaySound("coins_sound", MainGame.me.player_pos, true);
-                            var pos = MainGame.me.player_pos;
-                            pos.y += 125f;
-                            EffectBubblesManager.ShowImmediately(pos, $"{money}",
-                                num > 0 ? EffectBubblesManager.BubbleColor.Green : EffectBubblesManager.BubbleColor.Red,
-                                true, 4f);
-                        }
-                        else
-                        {
-                            Sounds.PlaySound("coins_sound", gerry2.pos3, true);
-                        }
-
-                        GJTimer.AddTimer(2f, delegate
-                        {
-                            gerry2.Say(strings.Bye, delegate
-                            {
-                                GJTimer.AddTimer(1f, delegate
-                                {
-                                    gerry2.ReplaceWithObject("talking_skull", true);
-                                    Tools.NameSpawnedGerry(gerry2);
-                                    gerry2.DestroyMe();
-
-                                    GJTimer.AddTimer(1f, delegate { ShowSummary(money); });
-                                });
-                            }, null, SpeechBubbleGUI.SpeechBubbleType.Talk, SmartSpeechEngine.VoiceID.Skull);
+                            DestroyGerryWithDelay(gerry2, 1f);
+                            GJTimer.AddTimer(1f, delegate { ShowSummary(money); });
                         });
-                    }, null, SpeechBubbleGUI.SpeechBubbleType.Talk,
-                    SmartSpeechEngine.VoiceID.Skull);
+                    });
+                });
             });
         });
     }
+
+    private static WorldGameObject SpawnGerry(Transform parent, Vector3 pos)
+    {
+        var spawnPos = new Vector3(pos.x, pos.y + 43f, pos.z);
+        var gerry = WorldMap.SpawnWGO(parent, "talking_skull", spawnPos);
+        Tools.NameSpawnedGerry(gerry);
+        gerry.ReplaceWithObject("talking_skull", true);
+        Tools.NameSpawnedGerry(gerry);
+
+        return gerry;
+    }
+
+    private static void DestroyGerryWithDelay(WorldGameObject gerry, float delay)
+    {
+        GJTimer.AddTimer(delay, delegate
+        {
+            gerry.ReplaceWithObject("talking_skull", true);
+            Tools.NameSpawnedGerry(gerry);
+            gerry.DestroyMe();
+        });
+    }
+
+    private static void PlayCoinsSoundAndShowMessage(float num, WorldGameObject gerry2, string money)
+    {
+        if (_showSoldMessagesOnPlayer.Value)
+        {
+            Sounds.PlaySound("coins_sound", MainGame.me.player_pos, true);
+            var pos = MainGame.me.player_pos;
+            pos.y += 125f;
+            EffectBubblesManager.ShowImmediately(pos, $"{money}",
+                num > 0 ? EffectBubblesManager.BubbleColor.Green : EffectBubblesManager.BubbleColor.Red,
+                true, 4f);
+        }
+        else
+        {
+            Sounds.PlaySound("coins_sound", gerry2.pos3, true);
+        }
+    }
+
 
     private static void TryAdd<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
     {
@@ -255,7 +272,9 @@ public partial class Plugin
 
     private static bool UnlockedFullPrice()
     {
-        return UnlockedShippingBoxExpansion() && MainGame.me.save.unlocked_techs.Exists(a => a.ToLowerInvariant().Equals("Best friend".ToLowerInvariant()));
+        return UnlockedShippingBoxExpansion() &&
+               MainGame.me.save.unlocked_techs.Exists(
+                   a => a.ToLowerInvariant().Equals("Best friend".ToLowerInvariant()));
     }
 
     private static bool UnlockedShippingBox()
@@ -266,27 +285,31 @@ public partial class Plugin
 
     private static bool UnlockedShippingBoxExpansion()
     {
-        return UnlockedShippingBox() && MainGame.me.save.unlocked_techs.Exists(a => a.ToLowerInvariant().Equals("Engineer".ToLowerInvariant()));
+        return UnlockedShippingBox() &&
+               MainGame.me.save.unlocked_techs.Exists(a => a.ToLowerInvariant().Equals("Engineer".ToLowerInvariant()));
     }
 
     private static void UpdateItemStates(ref ChestGUI instance)
     {
+        // Update player panel
         foreach (var inventory in instance.player_panel.multi_inventory.all.Where(i => i.data.inventory.Count > 0))
         {
-            // reset status
             foreach (var item in inventory.data.inventory)
             {
-                instance.player_panel.GetItemCellGuiForItem(item).SetInactiveState(false);
-            }
+                var itemCellGui = instance.player_panel.GetItemCellGuiForItem(item);
 
-            // disable quest item selling
-            foreach (var item in inventory.data.inventory.Where(item => item.definition.player_cant_throw_out && !ExcludeItems.Contains(item.definition.type)))
-            {
-                instance.player_panel.GetItemCellGuiForItem(item).SetInactiveState();
+                // Reset status
+                itemCellGui.SetInactiveState(false);
+
+                // Disable quest item selling
+                if (item.definition.player_cant_throw_out && !ExcludeItems.Contains(item.definition.type))
+                {
+                    itemCellGui.SetInactiveState();
+                }
             }
         }
 
-        // disable items in the chest inventory
+        // Disable items in the chest inventory
         foreach (var inventory in instance.chest_panel.multi_inventory.all.Where(i => i.data.inventory.Count > 0))
         {
             inventory.is_locked = true;
@@ -296,6 +319,7 @@ public partial class Plugin
             }
         }
     }
+
 
     private static int GetTrunkTier()
     {
